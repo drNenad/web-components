e319b94a103df82d7863a8dd08c84acac54f8c8a
Nenad Dragutinovic <32142868+drNenad@users.noreply.github.com>
2023-05-30 13:59:40 +0200

    Init repository

---
+++ src/axion-button.ts
@@ -0,0 +1,72 @@
+import { LitElement, css, html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+
+/**
+ * An example element.
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+@customElement('axion-button')
+export class AxionButton extends LitElement {
+  /**
+   * The number of times the button has been clicked.
+   */
+  @property({ type: String })
+  buttonType = 'primary'
+  /**
+   * The number of times the button has been clicked.
+   */
+  @property({ type: Boolean })
+  disabled = false
+  /**
+   * The number of times the button has been clicked.
+   */
+  @property({ type: Boolean })
+  loading = false
+
+  render() {
+    return html`
+      <button class="${this.buttonType}" disabled="${this.disabled}">
+        <slot></slot>
+      </button>
+    `;
+  }
+
+  static styles = css`
+    button {
+      display: inline-flex; 
+      transition-property: background-color, border-color, color, fill, stroke; 
+      transition-duration: 200ms; 
+      transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1); 
+      font-size: 0.875rem;
+      line-height: 1.25rem; 
+      justify-content: center; 
+      align-items: center; 
+      border-radius: 0.25rem; 
+      border-width: 1px; 
+      --ring-color: transparent;
+      padding: 8px;
+      cursor: pointer;
+    }
+    .primary {
+      min-width: 7rem;
+      border-color: #5b9eff; /* Replace with actual color code for signal-alt-base */
+      background-color: #5b9eff; /* Replace with actual color code for signal-alt-base */
+      color: white;
+    }
+
+    /* Add other styles for secondary, icon and extended classes. Replace with actual color code in each class */
+
+    .primary:disabled {
+      border-color: #8b8b8b; /* Replace with actual color code for signal-alt-low */
+      background-color: #8b8b8b; /* Replace with actual color code for signal-alt-low */
+    }
+  `;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'axion-button': AxionButton
+  }
+}
